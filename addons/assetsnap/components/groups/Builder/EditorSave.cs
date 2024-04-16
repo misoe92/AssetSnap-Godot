3e02e043501766975bfafc73102b577875192d5e
Misoe <mikebsorensen1@gmail.com>
2024-04-16 11:17:08 +0200

    cleanup

--- addons/assetsnap/components/groups/Builder/EditorSave.cs
+++ addons/assetsnap/components/groups/Builder/EditorSave.cs
@@ -24,7 +24,6 @@ namespace AssetSnap.Front.Components.Groups.Builder
 {
 	using AssetSnap.Component;
 	using Godot;
-	using Godot.Collections;
 
 	[Tool]
 	public partial class EditorSave : LibraryComponent
