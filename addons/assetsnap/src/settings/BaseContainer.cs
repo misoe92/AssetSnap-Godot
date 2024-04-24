e0733ef7c0ef89e9f292218eb3c2fe9c8544cb8c
Misoe <mikebsorensen1@gmail.com>
2024-04-24 10:30:37 +0200

    added new setting types

--- addons/assetsnap/src/settings/BaseContainer.cs
+++ addons/assetsnap/src/settings/BaseContainer.cs
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-namespace AssetSnap.Settings 
+namespace AssetSnap.Settings
 {
 	using System.Collections.Generic;
 	using AssetSnap.Front.Components;
@@ -38,9 +38,9 @@ namespace AssetSnap.Settings
 		private VBoxContainer SubContainerTwo;
 		private VBoxContainer SubContainerThree;
 		private VBoxContainer SubContainerFour;
-		
+
 		public bool Initialized = false;
-		
+
 		public BaseContainer()
 		{
 			Name = "SettingsBaseContainer";
@@ -54,7 +54,7 @@ namespace AssetSnap.Settings
 		*/
 		public void Initialize()
 		{
-			if( Initialized ) 
+			if (Initialized)
 			{
 				// Clear the instances first
 				_ScrollContainer.GetParent().RemoveChild(_ScrollContainer);
@@ -62,10 +62,10 @@ namespace AssetSnap.Settings
 			}
 
 			Initialized = true;
-			
+
 			SettingsConfig _Config = GlobalExplorer.GetInstance().Settings;
 			Name = "Settings";
-			
+
 			_ScrollContainer = new()
 			{
 				SizeFlagsVertical = Control.SizeFlags.ExpandFill,
@@ -82,31 +82,31 @@ namespace AssetSnap.Settings
 			_MarginContainer.AddThemeConstantOverride("margin_left", 5);
 			_MarginContainer.AddThemeConstantOverride("margin_right", 5);
 			_MarginContainer.AddThemeConstantOverride("margin_bottom", 5);
-			
+
 			_VBoxContainer = new()
 			{
 				SizeFlagsVertical = Control.SizeFlags.ExpandFill,
 				SizeFlagsHorizontal = Control.SizeFlags.ExpandFill
 			};
-			
+
 			_HBoxContainer = new()
 			{
 				SizeFlagsVertical = Control.SizeFlags.ShrinkBegin,
 				SizeFlagsHorizontal = Control.SizeFlags.ExpandFill
-			}; 
+			};
 
 			SubContainerOne = new()
 			{
 				SizeFlagsVertical = Control.SizeFlags.ShrinkBegin,
 				SizeFlagsHorizontal = Control.SizeFlags.ExpandFill
 			};
-			
+
 			SubContainerTwo = new()
 			{
 				SizeFlagsVertical = Control.SizeFlags.ShrinkBegin,
 				SizeFlagsHorizontal = Control.SizeFlags.ExpandFill
 			};
-			
+
 			SubContainerThree = new()
 			{
 				SizeFlagsVertical = Control.SizeFlags.ShrinkBegin,
@@ -118,9 +118,9 @@ namespace AssetSnap.Settings
 				SizeFlagsVertical = Control.SizeFlags.ShrinkBegin,
 				SizeFlagsHorizontal = Control.SizeFlags.ExpandFill
 			};
-			
+
 			RenderTypes();
-			
+
 			_HBoxContainer.AddChild(SubContainerOne);
 			_HBoxContainer.AddChild(SubContainerTwo);
 			_HBoxContainer.AddChild(SubContainerThree);
@@ -131,57 +131,57 @@ namespace AssetSnap.Settings
 			AddChild(_ScrollContainer);
 			StatesUtils.SetLoad("Settings", true);
 		}
- 
+
 		/*
 		** Fetches the setting type by method name
 		**
 		** @param string method
 		** @return string
 		*/
-		public string GetInputTypeByMethod( string method ) 
+		public string GetInputTypeByMethod(string method)
 		{
 			return (string)new Callable(this, method).Call();
 		}
-		
+
 		/*
 		** Checks if the given type is existing
 		**
 		** @param string str
 		** @return bool
 		*/
-		public bool HasInputTypeMethod( string str ) 
+		public bool HasInputTypeMethod(string str)
 		{
 			var Type = GetType();
-			return Type.GetMethod(str) != null; 
+			return Type.GetMethod(str) != null;
 		}
-		
+
 		/*
 		** Fetches the settings container
 		**
 		** @param int Iteration
 		** @return VBoxContainer
 		*/
-		private VBoxContainer GetSettingsContainer( int Iteration)
+		private VBoxContainer GetSettingsContainer(int Iteration)
 		{
 			VBoxContainer EntryContainer = SubContainerOne;
-			if( Iteration == 1 ) 
+			if (Iteration == 1)
 			{
-				EntryContainer = SubContainerTwo; 
+				EntryContainer = SubContainerTwo;
 			}
-			
-			if( Iteration == 2 )
+
+			if (Iteration == 2)
 			{
-				EntryContainer = SubContainerThree; 
+				EntryContainer = SubContainerThree;
 			}
-			
-			if( Iteration == 3 )
+
+			if (Iteration == 3)
 			{
-				EntryContainer = SubContainerFour; 
+				EntryContainer = SubContainerFour;
 			}
 
 			return EntryContainer;
 		}
-		
+
 		/*
 		** Renders the various setting
 		**
@@ -191,49 +191,49 @@ namespace AssetSnap.Settings
 		{
 			var Iteration = 0;
 			SettingsConfig _Config = GlobalExplorer.GetInstance().Settings;
-			Godot.Collections.Dictionary<string, Variant> _Settings = _Config.GetSettings(); 
-			foreach ( (string key, Variant value) in _Settings ) 
+			Godot.Collections.Dictionary<string, Variant> _Settings = _Config.GetSettings();
+			foreach ((string key, Variant value) in _Settings)
 			{
-				string k = key + "_type"; 
-				if( HasInputTypeMethod(k) ) 
+				string k = key + "_type";
+				if (HasInputTypeMethod(k))
 				{
 					string InputType = GetInputTypeByMethod(k);
 					VBoxContainer EntryContainer = GetSettingsContainer(Iteration);
 
-					if( value is Godot.Variant VariantValue ) 
+					if (value is Godot.Variant VariantValue)
 					{
 						string StringValue = VariantValue.As<string>();
 						float FloatValue = VariantValue.As<float>();
-						
-						if( StringValue.ToLower() == "false" || StringValue.ToLower() == "true" )
+
+						if (StringValue.ToLower() == "false" || StringValue.ToLower() == "true")
 						{
-							bool FinalValue = false; 
-							
-							if( StringValue == "true" )  
+							bool FinalValue = false;
+
+							if (StringValue == "true")
 							{
 								FinalValue = true;
 							}
-							RenderBoolType(key, FinalValue, InputType, EntryContainer); 
+							RenderBoolType(key, FinalValue, InputType, EntryContainer);
 						}
-						else 
+						else
 						{
-							RenderIntegerType(key, FloatValue, InputType, EntryContainer); 
+							RenderIntegerType(key, FloatValue, InputType, EntryContainer);
 						}
 					}
-					else 
+					else
 					{
-						GD.PushWarning("Found no type matching:", value.GetType());	
+						GD.PushWarning("Found no type matching:", value.GetType());
 					}
 				}
-	
-				Iteration += 1; 
-				if( Iteration > 3 )
+
+				Iteration += 1;
+				if (Iteration > 3)
 				{
 					Iteration = 0;
 				}
-			} 
+			}
 		}
-		
+
 		/*
 		** Renders a string type setting
 		**
@@ -243,16 +243,16 @@ namespace AssetSnap.Settings
 		** @param VBoxContainer _Container
 		** @return void
 		*/
-		public void RenderStringType(string key, string value, string Type, VBoxContainer _Container) 
+		public void RenderStringType(string key, string value, string Type, VBoxContainer _Container)
 		{
-			switch( Type ) 
+			switch (Type)
 			{
 				case "LineEdit":
 
 					break;
 			}
 		}
-			
+
 		/*
 		** Renders a integer type setting
 		**
@@ -261,18 +261,18 @@ namespace AssetSnap.Settings
 		** @param string Type
 		** @param VBoxContainer _Container
 		** @return void
-		*/	
-		public void RenderIntegerType(string key, float value, string Type, VBoxContainer _Container) 
+		*/
+		public void RenderIntegerType(string key, float value, string Type, VBoxContainer _Container)
 		{
-			switch( Type ) 
+			switch (Type)
 			{
 				case "SpinBox":
 					List<string> Components = new()
 					{
 						"SettingsSpinBox",
 					};
-					
-					if (GlobalExplorer.GetInstance().Components.HasAll( Components.ToArray() )) 
+
+					if (GlobalExplorer.GetInstance().Components.HasAll(Components.ToArray()))
 					{
 						SettingsSpinBox _SettingsSpinBox = GlobalExplorer.GetInstance().Components.Single<SettingsSpinBox>(true);
 
@@ -284,7 +284,7 @@ namespace AssetSnap.Settings
 					break;
 			}
 		}
-		
+
 		/*
 		** Renders a boolable type setting
 		**
@@ -293,18 +293,18 @@ namespace AssetSnap.Settings
 		** @param string Type
 		** @param VBoxContainer _Container
 		** @return void
-		*/	
-		public void RenderBoolType(string key, bool value, string Type, VBoxContainer _Container) 
+		*/
+		public void RenderBoolType(string key, bool value, string Type, VBoxContainer _Container)
 		{
-			switch( Type ) 
+			switch (Type)
 			{
 				case "CheckBox":
 					List<string> Components = new()
 					{
 						"SettingsCheckbox",
 					};
-					
-					if (GlobalExplorer.GetInstance().Components.HasAll( Components.ToArray() )) 
+
+					if (GlobalExplorer.GetInstance().Components.HasAll(Components.ToArray()))
 					{
 						SettingsCheckbox _SettingsCheckbox = GlobalExplorer.GetInstance().Components.Single<SettingsCheckbox>(true);
 
@@ -316,53 +316,53 @@ namespace AssetSnap.Settings
 					break;
 			}
 		}
-			
+
 		/*
 		** Converts a key to a label
 		**
 		** @param string key
 		** @return string
-		*/	
-		public string KeyToLabel( string key ) 
+		*/
+		public string KeyToLabel(string key)
 		{
 			return key.Capitalize().Split('_').Join(" ");
 		}
-		
+
 		/*
 		** Defines the input type of allow multi drop
 		**
 		** @return string
-		*/	
+		*/
 		public string allow_multi_drop_type()
 		{
 			return "CheckBox";
 		}
-		
+
 		/*
 		** Defines the input type of allow model grab
 		**
 		** @return string
-		*/	
+		*/
 		public string allow_model_grab_type()
 		{
 			return "CheckBox";
 		}
-				
+
 		/*
 		** Defines the input type of add_collisions
 		**
 		** @return string
-		*/	
+		*/
 		public string add_collisions_type()
 		{
 			return "CheckBox";
 		}
-				
+
 		/*
 		** Defines the input type of boundary_box_opacity
 		**
 		** @return string
-		*/	
+		*/
 		public string boundary_box_opacity_type()
 		{
 			return "SpinBox";
@@ -372,42 +372,42 @@ namespace AssetSnap.Settings
 		** Defines the input type of focus_placed_asset
 		**
 		** @return string
-		*/	
+		*/
 		public string focus_placed_asset_type()
 		{
 			return "CheckBox";
 		}
-		
+
 		/*
 		** Defines the input type of use_as_overlay
 		**
 		** @return string
-		*/	
+		*/
 		public string use_as_overlay_type()
 		{
 			return "CheckBox";
 		}
-				
+
 		/*
 		** Defines the input type of push_to_scene
 		**
 		** @return string
-		*/	
+		*/
 		public string push_to_scene_type()
 		{
 			return "CheckBox";
 		}
-				
+
 		/*
 		** Defines the input type of show_snap_boundary_box
 		**
 		** @return string
-		*/	
+		*/
 		public string show_snap_boundary_box_type()
 		{
 			return "CheckBox";
 		}
-				
+
 		/*
 		** Defines the input type of allow_drag_add
 		**
@@ -417,13 +417,63 @@ namespace AssetSnap.Settings
 		{
 			return "CheckBox";
 		}
-		
+
 		/*
 		** Defines the input type of allow_group_builder
 		**
 		** @return string
 		*/
 		public string allow_group_builder_type()
+		{
+			return "CheckBox";
+		}
+
+		/*
+		** Defines the input type of allow_group_builder
+		**
+		** @return string
+		*/
+		public string enable_model_spawn_in_type()
+		{
+			return "CheckBox";
+		}
+
+		/*
+		** Defines the input type of allow_group_builder
+		**
+		** @return string
+		*/
+		public string initial_model_spawn_in_level_type()
+		{
+			return "SpinBox";
+		}
+
+		/*
+		** Defines the input type of allow_group_builder
+		**
+		** @return string
+		*/
+		public string model_preview_size_type()
+		{
+			return "SpinBox";
+		}
+
+		/*
+		** Defines the input type of allow_group_builder
+		**
+		** @return string
+		*/
+		public string model_spawn_in_duration_type()
+		{
+			return "SpinBox";
+		}
+
+		/*
+		** Defines the input type of allow_group_builder
+		**
+		** @return string
+		*/
+		public string boundary_box_flat_type()
 		{
 			return "CheckBox";
 		}
