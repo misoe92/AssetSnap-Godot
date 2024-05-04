0311b2bf3c967ec9fd5dbb124e7998622155c979
Misoe <mikebsorensen1@gmail.com>
2024-05-04 09:52:27 +0200

    restructured

--- addons/assetsnap/traits/Dropdownable.cs
+++ addons/assetsnap/traits/Dropdownable.cs
@@ -21,50 +21,41 @@
 // SOFTWARE.
 
 #if TOOLS
+
 using AssetSnap.Front.Nodes;
 using AssetSnap.Trait;
 using Godot;
+
 namespace AssetSnap.Component
 {
 	/// <summary>
-    /// A dropdownable component that extends ContainerTrait, allowing the creation of dropdown menus.
-    /// </summary>
+	/// A dropdownable component that extends ContainerTrait, allowing the creation of dropdown menus.
+	/// </summary>
 	[Tool]
 	public partial class Dropdownable : ContainerTrait
 	{
-		/*
-		** Enums
-		*/
 		public enum DropdownState
 		{
 			Hidden,
 			Shown
 		};
 
-		/*
-		** Exports
-		*/
 		[Export]
 		public Godot.Collections.Array<Button> Items = new();
 
-		/*
-		** Public
-		*/
-		public PanelContainer panelContainer;
-		public MarginContainer _PanelPaddingContainer;
-		public VBoxContainer panelInnerContainer;
+		
+		public PanelContainer PanelContainer;
+		public MarginContainer PanelPaddingContainer;
+		public VBoxContainer PanelInnerContainer;
 		public DropdownButton SelectedBlock;
 		public VBoxContainer ItemsInnerContainer;
 
-		/*
-		** Private
-		*/
-		private string DefaultValue = "";
-		private string PrefixLabel = "";
+		private string _DefaultValue = "";
+		private string _PrefixLabel = "";
 		
 		/// <summary>
-        /// Constructor for the Dropdownable class.
-        /// </summary>
+		/// Constructor for the Dropdownable class.
+		/// </summary>
 		public Dropdownable()
 		{
 			Name = "Dropdownable";
@@ -72,136 +63,9 @@ namespace AssetSnap.Component
 		}
 		
 		/// <summary>
-        /// Instantiate an instance of the trait.
-        /// </summary>
-        /// <param name="i">An integer value.</param>
-        /// <returns>Returns the instantiated Dropdownable instance.</returns>
-		public Dropdownable Instantiate(int i)
-		{
-			UsePaddingContainer = false;
-			base._Instantiate();
-			base.Instantiate();
-
-			panelContainer = new()
-			{
-				Name = "DropdownPanel",
-				ThemeTypeVariation = "DropdownPanel",
-			};
-			
-			_PanelPaddingContainer = new()
-			{
-				Name = "DropdownPanelPaddingContainer",
-				Visible = false,
-			};
-
-			foreach( (string side, int value ) in Padding ) 
-			{ 
-				_PanelPaddingContainer.AddThemeConstantOverride("margin_" + side, value);
-			}
-			
-			// Setup the dropdown
-			panelInnerContainer = new()
-			{
-				Name = "DropdownPanelContainer",
-			};
-			
-			SelectedBlock = new()
-			{
-				ThemeTypeVariation = "DropdownButton",
-				Name = "DropdownSelected",
-				MouseDefaultCursorShape = Control.CursorShape.PointingHand,
-				Icon = GD.Load<Texture2D>("res://addons/assetsnap/assets/icons/chevron-down.svg"),
-				IconAlignment = HorizontalAlignment.Right,
-				Alignment = HorizontalAlignment.Left,
-				Margin = Margin,
-			};
-			
-			if( "" != DefaultValue ) 
-			{
-				SelectedBlock.Text = DefaultValue;
-			}
-			else 
-			{
-				SelectedBlock.Text = "None";
-			}
-
-			ItemsInnerContainer = new()
-			{
-				Name = "DropdownInnerContainer",
-			};
-
-			// Setup the layout
-			_PanelPaddingContainer.AddChild(ItemsInnerContainer);
-			panelInnerContainer.AddChild(SelectedBlock);
-			panelInnerContainer.AddChild(_PanelPaddingContainer);
-			panelContainer.AddChild(panelInnerContainer);
-			base.GetInnerContainer(0).AddChild(panelContainer);
-			
-			Dependencies[TraitName + "_WorkingNode"] = panelContainer;
-			Dependencies[TraitName + "_PanelPaddingContainer"] = _PanelPaddingContainer;
-			Dependencies[TraitName + "_PanelInnerContainer"] = panelInnerContainer;
-			Dependencies[TraitName + "_SelectedBlock"] = SelectedBlock;
-			Dependencies[TraitName + "_ItemsInnerContainer"] = ItemsInnerContainer;
-
-			// Add the node to the nodes array
-			Plugin.Singleton.traitGlobal.AddInstance(Iteration, panelContainer, OwnerName, TypeString, Dependencies);
-			Plugin.Singleton.traitGlobal.AddName(Iteration, TraitName, OwnerName, TypeString);
- 
-			// Clear the trait
-			Reset();
-			Iteration += 1;
-			Dependencies = new();
-
-			return this;
-		}
-		
-		/// <summary>
-        /// Selects a placed button in the nodes array by index.
-        /// </summary>
-        /// <param name="index">The index of the button to select.</param>
-        /// <param name="debug">Optional parameter to enable debugging.</param>
-        /// <returns>Returns the updated Dropdownable instance.</returns>
-		public override Dropdownable Select(int index, bool debug = false)
-		{
-			base._Select(index);
-
-			if( false != Dependencies.ContainsKey(TraitName + "_WorkingNode")) 
-			{
-				Godot.Collections.Dictionary<string, Variant> dependencies = Plugin.Singleton.traitGlobal.GetDependencies(index, TypeString, OwnerName);
-				Dependencies = dependencies;
-			}
-
-			return this; 
-		}
-		
-		/// <summary>
-        /// Selects a placed button in the nodes array by name.
-        /// </summary>
-        /// <param name="name">The name of the button to select.</param>
-        /// <returns>Returns the updated Dropdownable instance.</returns>
-		public override Dropdownable SelectByName(string name)
-		{
-			base.SelectByName(name);
-
-			return this;
-		}
-		
-		/// <summary>
-        /// Adds a button node to the dropdown.
-        /// </summary>
-        /// <param name="button">The button to add.</param>
-        /// <returns>Returns the updated Dropdownable instance.</returns>
-		public Dropdownable AddItem( Button button )
-		{
-			Items.Add(button);
-
-			return this;
-		}
-		
-		/// <summary>
-        /// Adds the currently chosen dropdown to a specified container.
-        /// </summary>
-        /// <param name="Container">The container to which the dropdown will be added.</param>
+		/// Adds the currently chosen dropdown to a specified container.
+		/// </summary>
+		/// <param name="Container">The container to which the dropdown will be added.</param>
 		public void AddToContainer( Node Container ) 
 		{
 			if( null == Dependencies || false == Dependencies.ContainsKey(TraitName + "_MarginContainer") ) 
@@ -224,10 +88,137 @@ namespace AssetSnap.Component
 		}
 		
 		/// <summary>
-        /// Sets a name for the current dropdown.
-        /// </summary>
-        /// <param name="text">The name to set.</param>
-        /// <returns>Returns the updated Dropdownable instance.</returns>
+		/// Instantiate an instance of the trait.
+		/// </summary>
+		/// <param name="i">An integer value.</param>
+		/// <returns>Returns the instantiated Dropdownable instance.</returns>
+		public Dropdownable Instantiate(int i)
+		{
+			UsePaddingContainer = false;
+			base._Instantiate();
+			base.Instantiate();
+
+			PanelContainer = new()
+			{
+				Name = "DropdownPanel",
+				ThemeTypeVariation = "DropdownPanel",
+			};
+			
+			PanelPaddingContainer = new()
+			{
+				Name = "DropdownPanelPaddingContainer",
+				Visible = false,
+			};
+
+			foreach( (string side, int value ) in Padding ) 
+			{ 
+				PanelPaddingContainer.AddThemeConstantOverride("margin_" + side, value);
+			}
+			
+			// Setup the dropdown
+			PanelInnerContainer = new()
+			{
+				Name = "DropdownPanelContainer",
+			};
+			
+			SelectedBlock = new()
+			{
+				ThemeTypeVariation = "DropdownButton",
+				Name = "DropdownSelected",
+				MouseDefaultCursorShape = Control.CursorShape.PointingHand,
+				Icon = GD.Load<Texture2D>("res://addons/assetsnap/assets/icons/chevron-down.svg"),
+				IconAlignment = HorizontalAlignment.Right,
+				Alignment = HorizontalAlignment.Left,
+				Margin = Margin,
+			};
+			
+			if( "" != _DefaultValue ) 
+			{
+				SelectedBlock.Text = _DefaultValue;
+			}
+			else 
+			{
+				SelectedBlock.Text = "None";
+			}
+
+			ItemsInnerContainer = new()
+			{
+				Name = "DropdownInnerContainer",
+			};
+
+			// Setup the layout
+			PanelPaddingContainer.AddChild(ItemsInnerContainer);
+			PanelInnerContainer.AddChild(SelectedBlock);
+			PanelInnerContainer.AddChild(PanelPaddingContainer);
+			PanelContainer.AddChild(PanelInnerContainer);
+			base.GetInnerContainer(0).AddChild(PanelContainer);
+			
+			Dependencies[TraitName + "_WorkingNode"] = PanelContainer;
+			Dependencies[TraitName + "_PanelPaddingContainer"] = PanelPaddingContainer;
+			Dependencies[TraitName + "_PanelInnerContainer"] = PanelInnerContainer;
+			Dependencies[TraitName + "_SelectedBlock"] = SelectedBlock;
+			Dependencies[TraitName + "_ItemsInnerContainer"] = ItemsInnerContainer;
+
+			// Add the node to the nodes array
+			Plugin.Singleton.TraitGlobal.AddInstance(Iteration, PanelContainer, OwnerName, TypeString, Dependencies);
+			Plugin.Singleton.TraitGlobal.AddName(Iteration, TraitName, OwnerName, TypeString);
+ 
+			// Clear the trait
+			Reset();
+			Iteration += 1;
+			Dependencies = new();
+
+			return this;
+		}
+		
+		/// <summary>
+		/// Selects a placed button in the nodes array by index.
+		/// </summary>
+		/// <param name="index">The index of the button to select.</param>
+		/// <param name="debug">Optional parameter to enable debugging.</param>
+		/// <returns>Returns the updated Dropdownable instance.</returns>
+		public override Dropdownable Select(int index, bool debug = false)
+		{
+			base._Select(index);
+
+			if( false != Dependencies.ContainsKey(TraitName + "_WorkingNode")) 
+			{
+				Godot.Collections.Dictionary<string, Variant> dependencies = Plugin.Singleton.TraitGlobal.GetDependencies(index, TypeString, OwnerName);
+				Dependencies = dependencies;
+			}
+
+			return this; 
+		}
+		
+		/// <summary>
+		/// Selects a placed button in the nodes array by name.
+		/// </summary>
+		/// <param name="name">The name of the button to select.</param>
+		/// <returns>Returns the updated Dropdownable instance.</returns>
+		public override Dropdownable SelectByName(string name)
+		{
+			base.SelectByName(name);
+
+			return this;
+		}
+		
+		/// <summary>
+		/// Adds a button node to the dropdown.
+		/// </summary>
+		/// <param name="button">The button to add.</param>
+		/// <returns>Returns the updated Dropdownable instance.</returns>
+		public Dropdownable AddItem( Button button )
+		{
+			Items.Add(button);
+
+			return this;
+		}
+		
+		/// <summary>
+		/// Sets a name for the current dropdown.
+		/// </summary>
+		/// <param name="text">The name to set.</param>
+		/// <returns>Returns the updated Dropdownable instance.</returns>
 		public Dropdownable SetName( string text ) 
 		{
 			base._SetName(text);
@@ -236,35 +227,35 @@ namespace AssetSnap.Component
 		}
 		
 		/// <summary>
-        /// Sets a prefix for the current dropdown.
-        /// </summary>
-        /// <param name="text">The prefix to set.</param>
-        /// <returns>Returns the updated Dropdownable instance.</returns>
+		/// Sets a prefix for the current dropdown.
+		/// </summary>
+		/// <param name="text">The prefix to set.</param>
+		/// <returns>Returns the updated Dropdownable instance.</returns>
 		public Dropdownable SetPrefix( string text ) 
 		{
-			PrefixLabel = text;
+			_PrefixLabel = text;
 
 			return this;
 		}
 		
 		/// <summary>
-        /// Sets the default value for the dropdown.
-        /// </summary>
-        /// <param name="value">The default value to set.</param>
-        /// <returns>Returns the updated Dropdownable instance.</returns>
+		/// Sets the default value for the dropdown.
+		/// </summary>
+		/// <param name="value">The default value to set.</param>
+		/// <returns>Returns the updated Dropdownable instance.</returns>
 		public Dropdownable SetDefaultValue(string value) 
 		{
-			DefaultValue = value;
+			_DefaultValue = value;
 			
 			return this;
 		}
 		
 		/// <summary>
-        /// Sets margin values for the currently chosen dropdown.
-        /// </summary>
-        /// <param name="value">The margin value.</param>
-        /// <param name="side">The side for which to set the margin.</param>
-        /// <returns>Returns the updated Dropdownable instance.</returns>
+		/// Sets margin values for the currently chosen dropdown.
+		/// </summary>
+		/// <param name="value">The margin value.</param>
+		/// <param name="side">The side for which to set the margin.</param>
+		/// <returns>Returns the updated Dropdownable instance.</returns>
 		public override Dropdownable SetMargin( int value, string side = "" ) 
 		{
 			base.SetMargin(value, side);
@@ -273,11 +264,11 @@ namespace AssetSnap.Component
 		}
 		
 		/// <summary>
-        /// Sets padding values for the currently chosen dropdown.
-        /// </summary>
-        /// <param name="value">The padding value.</param>
-        /// <param name="side">The side for which to set the padding.</param>
-        /// <returns>Returns the updated Dropdownable instance.</returns>
+		/// Sets padding values for the currently chosen dropdown.
+		/// </summary>
+		/// <param name="value">The padding value.</param>
+		/// <param name="side">The side for which to set the padding.</param>
+		/// <returns>Returns the updated Dropdownable instance.</returns>
 		public override Dropdownable SetPadding( int value, string side = "" ) 
 		{
 			base.SetPadding(value, side);
@@ -286,9 +277,9 @@ namespace AssetSnap.Component
 		}
 		
 		/// <summary>
-        /// Returns the inner container of the dropdown.
-        /// </summary>
-        /// <returns>Returns the inner container.</returns>
+		/// Returns the inner container of the dropdown.
+		/// </summary>
+		/// <returns>Returns the inner container.</returns>
 		public Container GetDropdownContainer()
 		{
 			if( null == Dependencies || false == Dependencies.ContainsKey(TraitName + "_ItemsInnerContainer") ) 
@@ -300,15 +291,15 @@ namespace AssetSnap.Component
 		}
 				
 		/// <summary>
-        /// Resets the trait to a cleared state.
-        /// </summary>
+		/// Resets the trait to a cleared state.
+		/// </summary>
 		protected override void Reset()
 		{
-			panelContainer = null;
-			panelInnerContainer = null;
+			PanelContainer = null;
+			PanelInnerContainer = null;
 			SelectedBlock = null;
 			ItemsInnerContainer = null;
-			_PanelPaddingContainer = null;
+			PanelPaddingContainer = null;
 
 			Dependencies = new();
 
