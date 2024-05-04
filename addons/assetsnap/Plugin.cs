42bb2ee08e753f414d51815664b729e66cf82c85
Misoe <mikebsorensen1@gmail.com>
2024-05-04 09:31:44 +0200

    Update Plugin.cs

--- addons/assetsnap/Plugin.cs
+++ addons/assetsnap/Plugin.cs
@@ -21,19 +21,19 @@
 // SOFTWARE.
 
 #if TOOLS
+
+using System;
+using AssetSnap.ASNode.Types;
+using AssetSnap.Core;
+using AssetSnap.Explorer;
+using AssetSnap.Front.Nodes;
+using AssetSnap.Nodes;
+using AssetSnap.States;
+using AssetSnap.Trait;
+using Godot;
+
 namespace AssetSnap
 {
-	using System;
-	using System.Collections.Generic;
-	using AssetSnap.ASNode.Types;
-	using AssetSnap.Core;
-	using AssetSnap.Explorer;
-	using AssetSnap.Front.Nodes;
-	using AssetSnap.Nodes;
-	using AssetSnap.States;
-	using AssetSnap.Trait;
-	using Godot;
-
 	[Tool]
 	public partial class Plugin : EditorPlugin, ISerializationListener
 	{
@@ -58,14 +58,24 @@ namespace AssetSnap
 		[Signal]
 		public delegate void ModelSizeCacheChangedEventHandler(string model, Vector3 value);
 
-		/** Internal data **/
-		private List<string> ignoredExtensions = new List<string> { ".png", ".jpg", ".jpeg", ".tga", ".bmp" }; // Add the extensions you want to ignore
-		private bool disposed = false;
-		private readonly string _Version = "0.1.2";
-		private readonly string _Name = "Plugin";
-
 		/* Bottom Dock */
-		public TraitGlobal traitGlobal
+		[Export]
+		public AsBottomDock Dock;
+		
+		/* The `public static Plugin Singleton` property is providing a way to access the singleton instance of
+		the `Plugin` class. By defining this property as static, it allows other parts of the code to access
+		the single instance of the `Plugin` class that is stored in the `_Instance` variable. This ensures
+		that there is only one instance of the `Plugin` class throughout the application, promoting a
+		singleton design pattern. */
+		public static Plugin Singleton
+		{
+			get
+			{
+				return _Instance;
+			}
+		}
+		
+		public TraitGlobal TraitGlobal
 		{
 			get
 			{
@@ -78,33 +88,26 @@ namespace AssetSnap
 			}
 			set
 			{
-				// GD.Print("Cant be set");
+				//
 			}
 		}
+		
+		/** Editor Node Types **/
+		public NodeType[] NodeTypes = Array.Empty<NodeType>();
+		
+		/** Internal data **/
+		protected Callable? _UpdateHandleCallable;
 
-		[Export]
-		public AsBottomDock _dock;
-
-		protected Callable? UpdateHandleCallable;
+		private static Plugin _Instance;
+		
+		private readonly string _Version = "0.1.2";
+		private readonly string _Name = "Plugin";
+		private bool _Disposed = false;
 		private bool _initialized = false;
-
-		/** Internal Components **/
 		private CoreEnter _CoreEnter = new();
 		private CoreInput _CoreInput = new();
 		private CoreProcess _CoreProcess = new();
 		private CoreHandles _CoreHandles = new();
-
-		/** Editor Node Types **/
-		public NodeType[] NodeTypes = Array.Empty<NodeType>();
-		private static Plugin _Instance;
-
-		public static Plugin Singleton
-		{
-			get
-			{
-				return _Instance;
-			}
-		}
 
 		public static Plugin GetInstance()
 		{
@@ -135,20 +138,29 @@ namespace AssetSnap
 		{
 			_Instance = this;
 
-			AddChild(traitGlobal);
+			AddChild(TraitGlobal);
+			
 			if ( false == HasInternalContainer() )
 			{
-				Node internalNode = new()
+				Node InternalNode = new()
 				{
 					Name = "InternalNode"
 				};
 
-				AddChild(internalNode);
+				AddChild(InternalNode);
 			}
 			
-			_dock = new AsBottomDock();
-			AddControlToBottomPanel(_dock, "Assets");
-			Connect(EditorPlugin.SignalName.SceneChanged, Callable.From((Node scene) => { _OnSceneChanged(scene); }));
+			Dock = new AsBottomDock();
+			AddControlToBottomPanel(Dock, "Assets");
+			
+			Connect(EditorPlugin.SignalName.SceneChanged, 
+				Callable.From(
+					(Node scene) =>
+					{
+						_OnSceneChanged(scene);
+					}
+				)
+			);
 
 			if (null == GlobalExplorer.InitializeExplorer())
 			{
@@ -156,8 +168,8 @@ namespace AssetSnap
 				return;
 			}
 
-			UpdateHandleCallable = new(this, "UpdateHandle");
-			if(false == IsUpdateHandleConnected()) 
+			_UpdateHandleCallable = new(this, "UpdateHandle");
+			if(false == _IsUpdateHandleConnected()) 
 			{
 				EditorInterface.Singleton.GetInspector().Connect(EditorInspector.SignalName.EditedObjectChanged, UpdateCallable());
 			}
@@ -174,51 +186,39 @@ namespace AssetSnap
 			ModelPreviewer.Singleton.Enter();
 			ModelPreviewer.Singleton.GeneratePreviews();
 		}
-
-		private void DoRemoveFolder(string title)
+		
+		/// <summary>
+		/// This C# function overrides the _Process method to tick the _CoreProcess object if it is not null and
+		/// not disposed.
+		/// </summary>
+		/// <param name="delta">The `delta` parameter in the `_Process` method represents the time elapsed in
+		/// seconds since the last frame. It is typically used to update the game logic and perform actions that
+		/// need to be executed every frame, such as moving objects, updating animations, or handling
+		/// input.</param>
+		/// <returns>
+		/// In the provided code snippet, the `return` statement is being used to exit the method `_Process`
+		/// early under certain conditions. If either `_CoreProcess` is `null` or the variable `disposed` is
+		/// `true`, the method will return without executing the remaining code.
+		/// </returns>
+		public override void _Process(double delta)
 		{
-			ExplorerUtils.Get().Settings.RemoveFolder(title);
-		}
+			if (_CoreProcess == null || _Disposed)
+			{
+				return;
+			}
 
-		public bool HasInternalContainer()
-		{
-			return HasNode("InternalNode") && IsInstanceValid(GetNode("InternalNode"));
+			_CoreProcess.Tick(delta);
+
+			return;
 		}
 		
-		public Node GetInternalContainer()
-		{
-			if( false == HasInternalContainer() )
-			{
-				return null;
-			}
-			
-			return GetNode("InternalNode");
-		}
-
-		private void _OnSceneChanged(Node Scene)
-		{
-			StatesUtils.Get().CurrentScene = Scene;
-
-			if (StatesUtils.Get().CurrentScene == null)
-			{
-				_dock._TabContainer.Visible = false;
-				_dock._LoadingContainer.Visible = true;
-			}
-			else
-			{
-				_dock._TabContainer.Visible = true;
-				_dock._LoadingContainer.Visible = false;
-			}
-		}
-
-		/*
-		** Destruction of our plugin
-		**
-		** @return void 
-		*/
+		/// <summary>
+		/// The _ExitTree function disposes of various custom node types, frees nodes in dispose queues,
+		/// removes and frees controls, and queues free the internal container and traitGlobal.
+		/// </summary>
 		public override void _ExitTree()
 		{
-			disposed = true;
+			_Disposed = true;
 			
 			/** Initialize custom node types **/  
 			new ASNode.Types.AsNodeType().Dispose( this );
@@ -233,7 +233,7 @@ namespace AssetSnap
 			new ASNode.Types.AsOptimizedMultiMeshGroupType().Dispose( this );
 			new ASNode.Types.AsMultiMeshType().Dispose( this );
 			
-			foreach (GodotObject _object in traitGlobal.DisposeQueue)
+			foreach (GodotObject _object in TraitGlobal.DisposeQueue)
 			{
 				if (IsInstanceValid(_object) && _object is Node node)
 				{
@@ -263,15 +263,15 @@ namespace AssetSnap
 				{
 					if (IsInstanceValid(gobject) && gobject is Node node)
 					{
-						node.Free();
+						node.QueueFree();
 					}
 				}
 			}
 
-			if (null != _dock)
+			if (null != Dock)
 			{
-				RemoveControlFromBottomPanel(_dock);
-				_dock.Free();
+				RemoveControlFromBottomPanel(Dock);
+				Dock.QueueFree();
 			}
 			
 			if ( HasInternalContainer() && null != GetInternalContainer())
@@ -279,67 +279,174 @@ namespace AssetSnap
 				GetInternalContainer().QueueFree();
 			}
 
-			if (null != traitGlobal)
+			if (null != TraitGlobal)
 			{
-				traitGlobal.Free();
+				TraitGlobal.QueueFree();
 			}
 		}
-
-		/*
-		** Handling of communication with editor handles
-		** 
-		** @return bool 
-		*/
-		public override bool _Handles(GodotObject _object)
-		{
-			if (_CoreHandles == null || disposed)
-			{
-				return true;
-			}
-
-			return _CoreHandles.Handle(_object);
-		}
-
-		/*
-		** Handling of GUI Input in the editor.
-		**
-		** @return int
-		*/
+		
+		/// <summary>
+		/// This C# function overrides a method to handle 3D GUI input using a specified camera and input event.
+		/// </summary>
+		/// <param name="Camera3D">The `Camera3D` parameter represents a 3D camera used for rendering and
+		/// displaying the scene in a 3D environment. It typically contains information such as the camera's
+		/// position, orientation, field of view, and projection matrix. This parameter is used in the
+		/// `_Forward3DGuiInput</param>
+		/// <param name="InputEvent">An object representing an input event, such as a mouse click or keyboard
+		/// press.</param>
+		/// <returns>
+		/// The method is returning the result of calling the `Handle` method on the `_CoreInput` object with
+		/// the `camera` and `@event` parameters.
+		/// </returns>
 		public override int _Forward3DGuiInput(Camera3D camera, InputEvent @event)
 		{
 			// If internal component is not set
-			if (_CoreInput == null || disposed)
+			if (_CoreInput == null || _Disposed)
 			{
 				return (int)EditorPlugin.AfterGuiInput.Pass;
 			}
 
 			return _CoreInput.Handle(camera, @event);
 		}
-
-		/*
-		** Handling process ticks
-		**
-		** @return void 
-		*/
-		public override void _Process(double delta)
+		
+		/// <summary>
+		/// The UpdateCallable function returns a Callable object.
+		/// </summary>
+		/// <returns>
+		/// A Callable object is being returned.
+		/// </returns>
+		public Callable UpdateCallable()
 		{
-			if (_CoreProcess == null || disposed)
+			return (Callable)_UpdateHandleCallable;
+		}
+		
+		/// <summary>
+		/// This C# function returns the internal container node named "InternalNode" if it exists, otherwise
+		/// it returns null.
+		/// </summary>
+		/// <returns>
+		/// The method `GetInternalContainer` is returning a `Node` object. If `HasInternalContainer()`
+		/// returns false, then it will return null. Otherwise, it will return the node with the name
+		/// "InternalNode".
+		/// </returns>
+		public Node GetInternalContainer()
+		{
+			if( false == HasInternalContainer() )
 			{
-				return;
+				return null;
 			}
-
-			_CoreProcess.Tick(delta);
-
-			return;
+			
+			return GetNode("InternalNode");
+		}
+		
+		/// <summary>
+		/// The GetTabContainer function returns the TabContainer from the _dock object.
+		/// </summary>
+		/// <returns>
+		/// The method `GetTabContainer` is returning the `_TabContainer` property of the `_dock` object.
+		/// </returns>
+		public TabContainer GetTabContainer()
+		{
+			return Dock._TabContainer;
+		}
+		
+		/// <summary>
+		/// The GetVersion function in C# returns the version of the software.
+		/// </summary>
+		/// <returns>
+		/// The method GetVersion() is returning the value stored in the variable _Version.
+		/// </returns>
+		public string GetVersion()
+		{
+			return _Version;
 		}
 
-		/*
-		** Used to check whether or not HandleNode is in focus,
-		** and if not perform an action
-		**  
-		** @return void
-		*/
-		protected void UpdateHandle()
+		/// <summary>
+		/// The GetVersionString function in C# returns the version string.
+		/// </summary>
+		/// <returns>
+		/// The method `GetVersionString()` is returning the `_Version` string.
+		/// </returns>
+		public string GetVersionString()
+		{
+			return _Version;
+		}
+		
+		/// <summary>
+		/// The _Handles function checks if _CoreHandles is valid and not disposed before calling its Handle
+		/// method on a GodotObject.
+		/// </summary>
+		/// <param name="GodotObject">the object currently being handled</param>
+		/// <returns>
+		/// The method is returning a boolean value. If `_CoreHandles` is null or the object has been
+		/// disposed, it returns `true`. Otherwise, it calls the `Handle` method on `_CoreHandles` passing
+		/// `_object` as a parameter and returns the result of that method call.
+		/// </returns>
+		public override bool _Handles(GodotObject _object)
+		{
+			if (_CoreHandles == null || _Disposed)
+			{
+				return true;
+			}
+
+			return _CoreHandles.Handle(_object);
+		}
+		
+		/// <summary>
+		/// The function `HasInternalContainer` checks if an internal node exists and is valid.
+		/// </summary>
+		/// <returns>
+		/// The method `HasInternalContainer()` is returning a boolean value. It returns `true` if the node
+		/// named "InternalNode" exists and is a valid instance, otherwise it returns `false`.
+		/// </returns>
+		public bool HasInternalContainer()
+		{
+			return HasNode("InternalNode") && IsInstanceValid(GetNode("InternalNode"));
+		}
+
+		/// <summary>
+		/// The function DoRemoveFolder removes a folder with the specified title from the settings using
+		/// ExplorerUtils in C#.
+		/// </summary>
+		/// <param name="title">The `title` parameter is a string that represents the title of the folder that
+		/// needs to be removed.</param>
+		private void _DoRemoveFolder(string title)
+		{
+			ExplorerUtils.Get().Settings.RemoveFolder(title);
+		}
+
+		/// <summary>
+		/// The _OnSceneChanged function updates the visibility of UI elements based on the current scene in
+		/// C#.
+		/// </summary>
+		/// <param name="Node">In the provided code snippet, the `_OnSceneChanged` method takes a parameter
+		/// `Node Scene`. This parameter represents a Node object that is passed to the method when the scene
+		/// is changed. The method then updates the visibility of `_dock._TabContainer` and
+		/// `_dock._LoadingContainer` based on</param>
+		private void _OnSceneChanged(Node Scene)
+		{
+			StatesUtils.Get().CurrentScene = Scene;
+
+			if (StatesUtils.Get().CurrentScene == null)
+			{
+				Dock._TabContainer.Visible = false;
+				Dock._LoadingContainer.Visible = true;
+			}
+			else
+			{
+				Dock._TabContainer.Visible = true;
+				Dock._LoadingContainer.Visible = false;
+			}
+		}
+
+		/// <summary>
+		/// The UpdateHandle function checks if an object is being edited and updates the handle accordingly.
+		/// </summary>
+		/// <returns>
+		/// If the condition `EditorInterface.Singleton.GetInspector().GetEditedObject() == null` is true, then
+		/// the method will return without performing any further actions.
+		/// </returns>
+		protected void _UpdateHandle()
 		{
 			GlobalExplorer explorer = GlobalExplorer.GetInstance();
 			if (EditorInterface.Singleton.GetInspector().GetEditedObject() == null)
@@ -374,51 +481,25 @@ namespace AssetSnap
 			}
 		}
 
-		public Callable UpdateCallable()
+		/// <summary>
+		/// The function checks if the update handle is connected to the editor inspector for edited object
+		/// changes.
+		/// </summary>
+		/// <returns>
+		/// The method `IsUpdateHandleConnected` returns a boolean value indicating whether the update handle
+		/// is connected. If the `UpdateHandleCallable` is an instance of `Callable`, it checks if the
+		/// `EditorInspector` is connected to the `EditedObjectChanged` signal with the callable object. If
+		/// the conditions are met, it returns `true`, otherwise it returns `false`.
+		/// </returns>
+		protected bool _IsUpdateHandleConnected()
 		{
-			return (Callable)UpdateHandleCallable;
-		}
-
-		/*
-		** Checks if the update handle method is connected
-		**
-		** @return void
-		*/
-		protected bool IsUpdateHandleConnected()
-		{
-			if (UpdateHandleCallable is Callable callable)
+			if (_UpdateHandleCallable is Callable callable)
 			{
 				EditorInspector Inspector = EditorInterface.Singleton.GetInspector();
 				return Inspector.IsConnected(EditorInspector.SignalName.EditedObjectChanged, callable);
 			}
 
 			return false;
-		}
-
-		public TabContainer GetTabContainer()
-		{
-			return _dock._TabContainer;
-			// AsBottomDock; 
-		}
-
-		/*
-		** Fetches the current plugin version
-		**
-		** @return double
-		*/
-		public string GetVersion()
-		{
-			return _Version;
-		}
-
-		/*
-		** Fetches the current plugin version in a string
-		**
-		** @return string
-		*/
-		public string GetVersionString()
-		{
-			return _Version;
 		}
 	}
 }
